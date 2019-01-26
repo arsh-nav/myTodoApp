bab80015f1a2b41b90e357ada2fffbf5b6ea405d
arsh-nav <arshia.nawab@gmail.com>
2019-01-27 00:25:42 +0330

    Updates

---
+++ precache-manifest.2825993ccd6dc1ee6b00f088d795493f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d255d14c0bde3cc2978",
+    "url": "/myTodoApp/static/css/main.4e30ef09.chunk.css"
+  },
+  {
+    "revision": "5d255d14c0bde3cc2978",
+    "url": "/myTodoApp/static/js/main.5d255d14.chunk.js"
+  },
+  {
+    "revision": "2604bada1464e93bf59e",
+    "url": "/myTodoApp/static/js/1.2604bada.chunk.js"
+  },
+  {
+    "revision": "d370010f724d9cb65594",
+    "url": "/myTodoApp/static/js/runtime~main.d370010f.js"
+  },
+  {
+    "revision": "a8c64601ea194f6d8f3a06de5cd5c446",
+    "url": "/myTodoApp/index.html"
+  }
+];
